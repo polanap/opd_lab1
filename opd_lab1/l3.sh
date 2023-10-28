082251ace4a6175131d07d92bca082aa231fc4ea
polanap <95124950+polanap@users.noreply.github.com>
2023-10-28 13:09:17 +0300

    Update l3.sh

--- opd_lab1/l3.sh
+++ opd_lab1/l3.sh
@@ -1,11 +1,23 @@
 cd lab0
 
+chmod -R 777 xatu8
+chmod 777 klang9
+
 ln -P vanillite1 xatu8/pidovevanillite
-ln -s vanillite1 xatu8/pidgeotvanillite
+ln -s $PWD/vanillite1 xatu8/pidgeotvanillite
 cp blaziken0 klang9/ninjaskblaziken
 cat xatu8/pidgeot xatu8/pidgeot > salamence2_89
 cp salamence2  xatu8/crobat
 ln -s xatu8 Copy_37
 cp -r xatu8 xatu8/magcargo
+
+chmod 335 xatu8/bisharp
+chmod 570 xatu8/shroomish
+chmod 570 xatu8/magcargo
+chmod 577 xatu8/crobat
+chmod 440 xatu8/pidove
+chmod 062 xatu8/pidgeot
+chmod 512 klang9
+chmod 751 xatu8
 
 echo task 3 completed
